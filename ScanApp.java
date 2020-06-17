737078756b5e60f59d9764627c0d3679600651e9
VacheslavSUR <65016969+VacheslavSUR@users.noreply.github.com>
2020-06-17 21:23:55 +0300

    Add files via upload

---
+++ ScanApp.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+
+public class ScanApp {
+    public static void main(String args[]) throws IOException {
+        Crawler crawler = new Crawler(args[0], Integer.parseInt(args[1]));
+        crawler.Scan();
+        System.out.println("Depth: " + Integer.parseInt(args[1]));
+        crawler.Show();
+    }
+}
